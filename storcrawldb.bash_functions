38f75dd2eb05e861a4ac948b7329ceb36e757195
Ben McGough <bmcgough@gmail.com>
2017-09-13 13:18:25 -0700

    re-factor - folder/owner list on stdin now

---
+++ storcrawldb.bash_functions
@@ -0,0 +1,235 @@
+## generate tag
+# my_tag=$(gen_tag [supplied tag])
+function gen_tag {
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    echo $1
+  else
+    date +%Y%m%d%H%M
+  fi
+}
+
+# usage
+function usage {
+  printf "\nUsage: %s [options]\n" "$0"
+  printf "    %-18.18s  %-18.18s  %-s\n" "Param" "Value" "Description"
+  printf "  Required:\n"
+  printf "    %-18.18s  %-18.18s  %-s\n" "--config-file" "$storcrawl_config" "storcrawldb.config file"
+  printf "    %-18.18s  %-18.18s  %-s\n" "--action" "start" "start, (crawl), cleanup, list-tags, remove-tag"
+  printf "  Optional:\n"
+  printf "    %-18.18s  %-18.18s  %-s\n" "--tag" "None" "tag target if action requires"
+  printf "    %-18.18s  %-18.18s  %-s\n" "--force" "no" "set to 'yes' to remove DB view"
+  printf "  The actions are:\n"
+  printf "    start - begin a scan - this is where you would normally begin a crawl\n"
+  printf "    (crawl) - run pwalk on a folder - this in an internally called action\n"
+  printf "    cleanup - run in the batch system at the end to tidy things\n"
+  printf "    list-tags - list all tags currently in DB\n"
+  printf "    remove-tag - remove all tables related to tag specified with --tag parameter from DB\n"
+  printf "    print-log - print logs for -tag parameter from DB\n"
+  printf "    print-report - print report for -tag parameter from DB\n"
+  printf "    print-folders - print folder list for --tag parameter from DB\n"
+  printf "    print-owner-report - print space by owner report for --tag parameter from DB\n"
+  printf "\n\n"
+  exit 1
+}
+
+# set up our environment based on the tag supplied
+# export_env $my_tag
+function export_env {
+  local my_tag="$1"
+  export CRAWL_TAG=${CRAWL_TAG:=$my_tag}
+  export CRAWL_FILE_TABLE=${CRAWL_FILE_TABLE:="${file_tbl}${CRAWL_TAG}"}
+  export CRAWL_FOLDER_TABLE=${CRAWL_FOLDER_TABLE:="${folder_tbl}${CRAWL_TAG}"}
+  export CRAWL_DB_LOG_TABLE=${CRAWL_DB_LOG_TABLE:="${log_tbl}${CRAWL_TAG}"}
+  export CRAWL_SOURCE_TABLE=${CRAWL_SOURCE_TABLE:="${source_tbl}${CRAWL_TAG}"}
+  export CRAWL_REPORT_TABLE=${CRAWL_REPORT_TABLE:="${report_tbl}${CRAWL_TAG}"}
+  export CRAWL_CSV_DIR=${CRAWL_CSV_DIR:="${csv_dir}${CRAWL_TAG}"}
+  export CRAWL_OUTPUT_DIR=${CRAWL_OUTPUT_DIR:="${output_dir}${CRAWL_TAG}"}
+  export CRAWL_VIEW=${CRAWL_VIEW:=$storcrawldb_view}
+  export CRAWL_DB_CMD=${CRAWL_DB_CMD:=$db_cmd}
+  export CRAWL_PWALK_CMD=${CRAWL_PWALK_CMD:=$pwalk_cmd}
+  export CRAWL_SCHEDULER_CMD=${CRAWL_SCHEDULER_CMD:=$scheduler_cmd}
+  export CRAWL_CSVQUOTE_CMD=${CRAWL_CVSQUOTE_CMD:=$csvquote_cmd}
+}
+
+function storcrawl_log {
+  ts=$(date +'%s')
+  tz=$(date +'%Z')
+  hn=$(hostname)
+
+  log_msg="{"
+  log_msg="$log_msg \"timestamp\":\"${ts}\""
+  log_msg="$log_msg, \"timezone\":\"${tz}\""
+  log_msg="$log_msg, \"hostname\":\"${hn}\""
+  log_msg="$log_msg, \"script\":\"${0}\""
+  log_msg="$log_msg, \"function\":\"${FUNCNAME}\""
+  log_msg="$log_msg, \"pid\":\"${$}\""
+
+  if [ ! -z "${SLURM_JOB_ID}" ]
+  then
+    log_msg="$log_msg, \"slurm_job_id\":\"${SLURM_JOB_ID}\""
+    log_msg="$log_msg, \"slurm_array_job_id\":\"${SLURM_ARRAY_JOB_ID}\""
+    log_msg="$log_msg, \"slurm_array_task_id\":\"${SLURM_ARRAY_TASK_ID}\""
+    log_msg="$log_msg, \"slurm_job_name\":\"${SLURM_JOB_NAME}\""
+    log_msg="$log_msg, \"slurm_job_partition\":\"${SLURM_JOB_PARTITION}\""
+    log_msg="$log_msg, \"slurm_nodename\":\"${SLURM_NODENAME}\""
+  fi
+
+  log_msg="${log_msg}, \"${1}\":\"${2}\""
+    
+  log_msg="${log_msg} }"
+
+  ${storcrawl_log_func} "${log_msg}"
+}
+
+function run_scripts {
+  for script in ${1}/*
+  do
+    if [ -f $script -a -x $script ]
+    then
+      echo "running ${script}..."
+      $script
+      if [ "$?" -eq "0" ]
+      then
+        echo "$script ran successfully"
+      else
+        error_exit "$script failed to run"
+      fi
+    fi
+  done
+}
+
+# listens on STDIN for: owner/tag, foldername
+# owner/tag is optional - any string on a line w/o a comma is foldername
+function build_folder_table {
+  while read line
+  do
+    case "$line" in
+    *,*)
+      folder=${line#*,}
+      owner=${line%,*}
+      ;;
+    *)
+      folder="$line"
+      ;;
+    esac
+    my_folder_id=$(add_folder $folder $owner)
+    echo "$folder added with id $my_folder_id and owner $owner"
+  done
+}
+
+# export=$(get_export ${folder_name})
+function get_export {
+  # returns device name for direct-attach storage
+  mySource=$(df --output=source "$1" | awk '! /Filesystem/ {print $1}')
+  echo $mySource | grep -q ':'
+  if [ $? -eq 0 ]
+  then
+    echo $mySource | awk -F: '{print $2}'
+  else
+    echo $mySource
+  fi
+}
+
+# server=$(get_server ${folder_name})
+function get_server {
+  myServer=$(df --output=source "$1" | awk '! /Filesystem/ {print $1}')
+  echo $myServer | grep -q ':'
+  if [ $? -eq 0 ]
+  then
+    echo $myServer | awk -F: '{print $1}'
+  else
+    hostname
+  fi
+}
+
+# add_folder ${folder_name} ${owner}
+function add_folder {
+  local folder="${1}"
+  local owner="${2}"
+  local server=$(get_server ${folder})
+  local export=$(get_export ${folder})
+  local fs_id=$(get_fs_id ${server} ${export})
+  db_add_folder ${folder} ${fs_id} ${owner}
+}
+
+# wrapper functions (call db for now)
+function get_folder_id {
+  db_get_folder_id ${@}
+}
+
+function get_folder_detail {
+  db_get_folder_detail ${@}
+}
+
+function get_fs_id {
+  db_get_fs_id ${@}
+}
+
+function run_crawl_jobs {
+  slurm_run_crawl_jobs ${@}
+}
+
+function run_cleanup_job {
+  slurm_run_cleanup_job ${@}
+}
+
+function build_exclusion_list_file {
+  db_build_exclusion_list_file ${@}
+}
+
+# check output dirs
+function check_output_dirs {
+  if [ ! -d "$CRAWL_CSV_DIR" ]
+  then
+    mkdir -p $CRAWL_CSV_DIR ||\
+      error_exit "Unable to create CSV dir $CRAWL_CSV_DIR"
+  fi
+  if [ ! -w "$CRAWL_CSV_DIR" ]
+  then
+    error_exit "Unable to write to CSV dir $CRAWL_CSV_DIR"
+  fi
+  if [ ! -d "$CRAWL_OUTPUT_DIR" ]
+  then
+    mkdir -p $CRAWL_OUTPUT_DIR ||\
+      error_exit "Unable to create OUTPUT dir $CRAWL_OUTPUT_DIR"
+  fi
+  if [ ! -w "$CRAWL_OUTPUT_DIR" ]
+  then
+    error_exit "Unable to write to OUTPUT dir $CRAWL_OUTPUT_DIR"
+  fi
+}
+
+function check_array {
+  if [[ -z $SLURM_ARRAY_TASK_ID ]]
+  then
+    error_exit "No array task id found - run as a slurm array job only!"
+  fi
+}
+
+function check_exec {
+  # `which` only returns for executables
+  if [ ! $(which $1) ]
+  then
+    error_exit "Unable to execute $1"
+  fi
+}
+
+# run_pwalk ${folder_name} ${output}
+function run_pwalk {
+  local folder_name="${1}"
+  local output_file="${2}"
+  local csv_out="${CRAWL_CSV_DIR}/${output_file}.csv"
+  local excl_file="${CRAWL_CSV_DIR}/${output_file}.exclusion_list"
+  build_exclusion_list_file "${folder_name}" "${excl_file}"
+  # run pwalk
+  $pwalk_cmd $pwalk_opts --exclude "${excl_file}" "${folder_name}" > "${csv_out}"
+  storcrawl_log "pwalk exit" "${?}"
+}
+
+# print error and exit
+function error_exit {
+  echo "${PROGNAME} (tag $CRAWL_TAG): $1" 1>&2
+  exit 1
+}
+
--- storcrawldb.sh
+++ storcrawldb.sh
@@ -7,10 +7,12 @@ DIRNAME=$(dirname $0)
 source $DIRNAME/storcrawldb.config
 
 ## source functions
-source $DIRNAME/storcrawldb.functions
-source $DIRNAME/storcrawldb.db_functions
+source $DIRNAME/storcrawldb.bash_functions
+source $DIRNAME/${db_functions_file}
+source $DIRNAME/${scheduler_functions_file}
 
-# check arguments
+# gather parameters
+# see usage in functions file for parameter definitions
 while [[ $# > 1 ]]
 do
 case $1 in
@@ -37,233 +39,102 @@ esac
 shift
 done
 
-## generate tag
-if [ -z "$specified_tag" ]
-then
-  my_tag=$(date +%Y%m%d%H%M)
-else
-  my_tag=$specified_tag
-fi
+# set up tag, then export env vars
+my_tag=$(gen_tag $specified_tag)
+export_env "$my_tag"
 
-export STORCRAWLDB_TAG=${STORCRAWLDB_TAG:=$my_tag}
-export STORCRAWLDB_START_PATH=${STORCRAWLDB_START_PATHS:=$start_paths}
-export STORCRAWLDB_FILE_TABLE=${STORCRAWLDB_FILE_TABLE:="${file_tbl}${STORCRAWLDB_TAG}"}
-export STORCRAWLDB_FOLDER_TABLE=${STORCRAWLDB_FOLDER_TABLE:="${folder_tbl}${STORCRAWLDB_TAG}"}
-export STORCRAWLDB_LOG_TABLE=${STORCRAWLDB_LOG_TABLE:="${log_tbl}${STORCRAWLDB_TAG}"}
-export STORCRAWLDB_SOURCE_TABLE=${STORCRAWLDB_SOURCE_TABLE:="${source_tbl}${STORCRAWLDB_TAG}"}
-export STORCRAWLDB_REPORT_TABLE=${STORCRAWLDB_REPORT_TABLE:="${report_tbl}${STORCRAWLDB_TAG}"}
-export STORCRAWLDB_CSV_DIR=${STORCRAWLDB_CSV_DIR:="${csv_dir}${STORCRAWLDB_TAG}"}
-export STORCRAWLDB_OUTPUT_DIR=${STORCRAWLDB_OUTPUT_DIR:="${output_dir}${STORCRAWLDB_TAG}"}
-export STORCRAWLDB_VIEW=${STORCRAWLDB_VIEW:=$storcrawldb_view}
+# functions here - main flow @end
+#  storcrawldb.sh is called by itself with the --action parameter
+#  to determine behavior
 
-## load PostgreSQL Environment Modules
-source ${lmod_init}
-module use ${modulefile_dir}
-module load ${psql_module}
-
-## basic checking
-#  exists: output_dir, csv_dir, scripts dirs, start_path
-#  db connection
-#  slurm commands
-
+# start - create tables, schedule jobs
 function storcrawl_start {
-  # initialize db
-  init_storcrawldb
-  # check for tag
-  check_tag
-  # check that last crawl finished
-  check_last_crawl
-  # init the crawl tables
-  init_crawl
-  storcrawl_log $FUNCNAME
-  # build the folder table
-  build_folder_table
-  get_job_array_size
-  # run before scripts
-  run_scripts "${before_script_dir}"
-  # check output dirs
-  if [ ! -d "$STORCRAWLDB_CSV_DIR" ]
-  then
-    mkdir -p $STORCRAWL_CSV_DIR ||\
-      error_exit "Unable to create CSV dir $STORCRAWLDB_CSV_DIR"
-  fi
-  if [ ! -w "$STORCRAWLDB_CSV_DIR" ]
-  then
-    error_exit "Unable to write to CSV dir $STORCRAWLDB_CSV_DIR"
-  fi
-  if [ ! -d "$STORCRAWLDB_OUTPUT_DIR" ]
-  then
-    mkdir -p $STORCRAWL_OUTPUT_DIR ||\
-      error_exit "Unable to create OUTPUT dir $STORCRAWLDB_OUTPUT_DIR"
-  fi
-  if [ ! -w "$STORCRAWLDB_OUTPUT_DIR" ]
-  then
-    error_exit "Unable to write to OUTPUT dir $STORCRAWLDB_OUTPUT_DIR"
-  fi
-  # run crawl batch
-  sbatch_job_name="${sbatch_job_name_prefix}${STORCRAWLDB_TAG}"
-  storcrawl_log "scheduling crawl jobs"
-  echo "sbatch maxarraysize is ${sbatch_maxarraysize}"
-  num_job_arrays=$((${STORCRAWLDB_JOB_ARRAY_SIZE}/${sbatch_maxarraysize}+1))
-  job_array_end_id="${sbatch_maxarraysize}"
-  for i in $(seq 1 $num_job_arrays)
-  do
-    if [ "$i" -eq "$num_job_arrays" ]
-    then
-      # the last job array batch
-      job_array_end_id=$((${STORCRAWLDB_JOB_ARRAY_SIZE}%${sbatch_maxarraysize}))
-    fi
-    jobid=$(sbatch --array="1-${job_array_end_id}%${sbatch_simultaneous_tasks}" \
-                   --partition="$sbatch_partition" \
-                   --mail-type="$sbatch_mail_type" \
-                   --mail-user="$sbatch_mail_user" \
-                   --time="$sbatch_time" \
-                   --cpus-per-task="$sbatch_cpus_per_task" \
-                   --job-name="$sbatch_job_name" \
-                   --output="${STORCRAWLDB_OUTPUT_DIR}/output_%a_%A.%J.out" \
-                   --wrap="$0 --action crawl")
-    if [ $? -eq 0 ]
-    then
-      echo "crawl array job launched - jobid ${jobid}"
-    else
-      error_exit "Ooops, unable to launch crawl jobs, exiting..."
-    fi
-  done
 
-  # run cleanup job
-  storcrawl_log "scheduling cleanup job"
-  jobid=$(sbatch --partition="$sbatch_partition" \
-                 --mail-type="$sbatch_mail_type" \
-                 --mail-user="$sbatch_mail_user" \
-                 --time="$sbatch_time" \
-                 --output="${STORCRAWLDB_OUTPUT_DIR}/output_housekeeper_%a.%J.out" \
-                 --cpus-per-task="$sbatch_cpus_per_task" \
-                 --job-name="$sbatch_job_name" \
-                 --dependency=singleton \
-                 --wrap="$0 --action cleanup")
-  echo "crawl tidy job launched - jobid ${jobid}"
+  echo "storcrawldb running with action start"
+  init_db
+  check_tag
+  check_last_crawl
+  init_crawl
+  storcrawl_log "action" "start"
+  build_folder_table
+  set_job_array_size
+  run_scripts "${before_script_dir}"
+  check_output_dirs
+  run_crawl_jobs
+  run_cleanup_job
+
 }
 
+# execute pwalk - this is what each job will run
 function storcrawl_pwalk {
 
-  my_name="$FUNCNAME (${SLURM_ARRAY_JOB_ID}.${SLURM_JOB_ID}) on $(hostname)"
+  echo "storcrawldb running with action crawl"
+  storcrawl_log "action" "crawl"
+  check_array
+  check_exec $CRAWL_DB_CMD
+  check_exec $CRAWL_SCHEDULER_CMD
+  check_exec $CRAWL_PWALK_CMD
+  check_exec $CRAWL_CSVQUOTE_CMD
 
-  storcrawl_log "$my_name"
+  id=$(get_folder_id)
+  name=$(get_folder_detail ${id} "folder")
+  owner=$(get_folder_detail ${id} "owner")
+  fs_id=$(get_folder_detail ${id} "fs_id")
+  out="${fs_id}_$(echo ${name} | tr '/' '_')"
 
-  # checks
-  if [[ -z $SLURM_ARRAY_TASK_ID ]]
-  then
-    storcrawl_log "$my_name: no SLURM_ARRAY_TASK_ID found, aborting"
-    error_exit "You need to run this script as a slurm array job"
-  fi
+  echo "storcrawldb running pwalk on folder ${name} to output ${out}"
+  run_pwalk ${name} ${out}
+  import_crawl_csv ${out} ${fs_id} ${owner}
+  folder_crawl_finish ${id}
 
-  if [ ! $(which $owner_script) ]
-  then
-    storcrawl_log "$my_name: owner script $owner_script bad, aborting"
-    error_exit "Owner script $owner_script could not be executed"
-  fi
-
-  # `which` only returns for executables
-  if [ ! $(which $db_cli) ] || [ ! $(which $sbatch_cmd) ] || [ ! $(which $pwalk_cmd) ]
-  then
-    storcrawl_log "$my_name: bad db_cli: $db_cli, sbatch_cmd: $sbatch_cmd, or pwalk_cmd: $pwalk_cmd, aborting"
-    error_exit "Unable to execute $db_cli, $sbatch_cmd, or $pwalk_cmd"
-  fi
-    
-  # this sets pwalk_start_path
-  get_folder
-  storcrawl_log "$my_name: got folder $pwalk_start_path"
-
-  # determine owner
-  my_owner=$($owner_script $pwalk_start_path)
-  storcrawl_log "$my_name: got owner $my_owner"
-
-  # determine server/export
-  # this sets $pwalk_export
-  get_export "$pwalk_start_path"
-  # this sets $pwalk_server
-  get_server "$pwalk_start_path"
-
-  storcrawl_log "$my_name: ${pwalk_server}:${pwalk_export} - getting source id"
-
-  # get source_id - this sets $pwalk_source_id
-  get_source_id "$pwalk_server" "$pwalk_export"
-
-  # create csv (data) filename - needs to have folder name, but also source id to avoid collisions
-  folder_path_filename="$(echo $pwalk_start_path | tr '/' '_')"
-  csv_filename="${pwalk_source_id}${folder_path_filename}.csv"
-
-  storcrawl_log "$my_name: source id $pwalk_source_id (${pwalk_server}:${pwalk_export})"
-
-  $pwalk_cmd --NoSnap --maxthreads=32 $pwalk_start_path > "${STORCRAWLDB_CSV_DIR}/${csv_filename}"
-  storcrawl_log "$my_name done with pwalk"
-
-  # the output of pwalk (15 fields)
-  #   inode, parent_inode, directory_depth, filename, fileextension, UID, GID, st_size, st_blocks, st_mode,
-  #   st_atime, st_mtime, st_ctime, count, sum
-  #
-  # [count is the count of inodes contained in the directory, or -1 if not a directory
-  #  sum in the sum in bytes of all files in the directory]
-
-  # importing to file table
-  storcrawl_log "$my_name importing file table"
-  ${csvquote_cmd} "${STORCRAWLDB_CSV_DIR}/$csv_filename" | uconv -s -i | sort -t, -k 4,4 -u | awk -F, -v fs_source_id="${pwalk_source_id}" -v owner="${my_owner}" '{print fs_source_id","owner","$0}' | ${csvquote_cmd} -u | psql $db_conn_str -c "COPY ${STORCRAWLDB_FILE_TABLE}(fs_id,owner,st_ino,parent_inode,directory_depth,filename,fileextension,st_uid,st_gid,st_size,st_blocks,st_mode,st_atime,st_mtime,st_ctime,count,sum) FROM STDIN WITH csv ESCAPE '\'"
-
-  if [ $? -eq 0 ]
-  then 
-    storcrawl_log "$my_name successfully ran COPY"
-  else
-    storcrawl_log "$my_name failed during COPY - see ${STORCRAWLDB_OUTPUT_DIR}/output_${SLURM_ARRAY_TASK_ID}_${SLURM_ARRAY_JOB_ID}.${SLURM_JOB_ID}.out"
-    error_exit "COPY failed"
-  fi
-
-  # update for folder
-  folder_crawl_finish
-
-  storcrawl_log "$my_name all done"
 } 
 
+# known as cleanup, this is run by one job at the end of the crawl
 function storcrawl_tidy {
-  storcrawl_log $FUNCNAME
-  # gererate report
+
+  echo "storcrawldb running with action tidy"
+  storcrawl_log "action" "tidy"
   generate_report
-  # update ro user
   update_ro_grants
-  # update view
   create_storcrawldb_views
-  # check for old tables and clean
   clean_tables
   # archive here
-  # run after scripts
   run_scripts "${after_script_dir}"
+
 }
 
+# display all knowns tags
 function list_tags_from_db {
   list_tags
   exit
 }
 
+# delete all tables associated with a tag
 function remove_tag_from_db {
   remove_tag $specified_tag
+  echo "note that directories associated with this tag are not removed"
   exit
 }
 
+# dump the log for a given tag
 function print_log_for_tag {
   print_log
   exit
 }
 
+# dump the report for a given tag
 function print_report_for_tag {
   print_report
   exit
 }
 
+# dump the folder/owner list of a given tag
 function print_folders_for_tag {
   print_folders
   exit
 }
 
-## main tree
+## main flow
 if [ "$storcrawl_action" = "start" ]
 then
   storcrawl_start
